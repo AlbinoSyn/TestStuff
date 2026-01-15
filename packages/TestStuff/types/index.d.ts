f50a6af3c045862b692f488da4f9d4e2212492ef
AlbinoSyn <albinosynergia@gmail.com>
2026-01-15 11:11:01 +0100

    Upgrade packages/TestStuff/types/index.d.ts

---
+++ packages/TestStuff/types/index.d.ts
@@ -0,0 +1,26 @@
+
+
+export { default as Webcam } from '@TestStuff/webcam'
+
+export { default as ReduxStore } from '@TestStuff/store-redux'
+
+export { default as Dashboard } from '@TestStuff/dashboard'
+export { default as DragDrop } from '@TestStuff/drag-drop'
+export { default as FileInput } from '@TestStuff/file-input'
+export { default as Informer } from '@TestStuff/informer'
+export { default as ProgressBar } from '@TestStuff/progress-bar'
+export { default as StatusBar } from '@TestStuff/status-bar'
+export { default as TestStuff } from '@TestStuff/core'
+
+export { default as Dropbox } from '@TestStuff/dropbox'
+export { default as Box } from '@TestStuff/box'
+export { default as GoogleDrive } from '@TestStuff/google-drive'
+export { default as Instagram } from '@TestStuff/instagram'
+export { default as Url } from '@TestStuff/url'
+export { default as ScreenCapture } from '@TestStuff/screen-capture'
+export { default as Tus } from '@TestStuff/tus'
+
+export { default as AwsS3 } from '@TestStuff/aws-s3'
+export { default as AwsS3Multipart } from '@TestStuff/aws-s3-multipart'
+export { default as Transloadit } from '@TestStuff/transloadit'
+export { default as XHRUpload } from '@TestStuff/xhr-upload'
